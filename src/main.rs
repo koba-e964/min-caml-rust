d8655eb9db5ae6820b8bd8c70c339f8e24ae8d9b
koba-e964 <koba_max@kobas-Air.pohe>
2017-02-21 17:29:58 +0900

    Port type.ml, syntax.ml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate min_caml_rust;
+
+use min_caml_rust::syntax::*;
 fn main() {
     println!("Mitou Min-Caml Compiler (C) Eijiro Sumii\n (Port to Rust)");
+    println!("{:?}", Syntax::Unit);
 }
---
+++ src/syntax.rs
@@ -0,0 +1,57 @@
+#[derive(Clone, Debug)]
+// syntax.ml
+pub enum Syntax {
+    Unit,
+    Bool(bool),
+    Int(i64),
+    Float(f64),
+    Not(Box<Syntax>),
+    Neg(Box<Syntax>),
+    IntBin(IntBin, Box<Syntax>, Box<Syntax>),
+    FNeg(Box<Syntax>),
+    FloatBin(FloatBin, Box<Syntax>, Box<Syntax>),
+    CompBin(CompBin, Box<Syntax>, Box<Syntax>),
+    If(Box<Syntax>, Box<Syntax>, Box<Syntax>),
+    Let((String, Type), Box<Syntax>, Box<Syntax>),
+    Var(String),
+    LetRec(Fundef, Box<Syntax>),
+    App(Box<Syntax>, Box<[Syntax]>),
+    Tuple(Box<[Syntax]>),
+    LetTuple(Box<[(String, Type)]>, Box<Syntax>, Box<Syntax>),
+    Array(Box<Syntax>, Box<Syntax>),
+    Get(Box<Syntax>, Box<Syntax>),
+    Put(Box<Syntax>, Box<Syntax>, Box<Syntax>),
+}
+
+#[derive(Clone, Debug)]
+pub struct Fundef {
+    name: (String, Type),
+    args: Box<[(String, Type)]>,
+    body: Box<Syntax>,
+}
+
+#[derive(Clone, Debug)]
+pub enum IntBin {
+    Add, Sub,
+}
+#[derive(Clone, Debug)]
+pub enum FloatBin {
+    FAdd, FSub, FMul, FDiv,
+}
+#[derive(Clone, Debug)]
+pub enum CompBin {
+    Eq, LE,
+}
+
+// type.ml
+#[derive(Clone, Debug)]
+pub enum Type {
+    Unit,
+    Bool,
+    Int,
+    Float,
+    Fun(Box<[Type]>, Box<Type>),
+    Tuple(Box<[Type]>),
+    Array(Box<Type>),
+    Var(Option<Box<Type>>),
+}
