cbd9fb887cd6d86d4343c29462b046c8b16160a4
koba-e964 <koba_max@kobas-Air.pohe>
2017-02-26 20:03:13 +0900

    Fix gen_tmp in id.rs (use t:&Type to determine the prefix)

--- src/id.rs
+++ src/id.rs
@@ -17,8 +17,20 @@ impl IdGen {
         let a = self.gen();
         Type::Var(a)
     }
+    fn id_of_typ(t: &Type) -> String {
+        match *t {
+            Type::Unit => "u",
+            Type::Bool => "b",
+            Type::Int => "i",
+            Type::Float => "d",
+            Type::Fun(_, _) => "f",
+            Type::Tuple(_) => "t",
+            Type::Array(_) => "a",
+            Type::Var(_) => panic!("Var is not appropriate"),
+        }.to_string()
+    }
     pub fn gen_tmp(&mut self, t: &Type) -> String {
         let a = self.gen();
-        format!("v{}", a)
+        format!("{}{}", Self::id_of_typ(t), a)
     }
 }
